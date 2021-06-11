f96b5572281561e296e7266c05dbcee7d826deff
DARKSIDEOPG <28642811+DARKSIDEOPG@users.noreply.github.com>
2021-06-11 16:20:05 +0300

    Update to 1.0.0.2

---
+++ Source/Downloader.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace SFTool
+{
+    public class Downloader
+    {
+        // Resulting arrays. Used in GetConfig.
+        Dictionary<string, string> modelData = new Dictionary<string, string>();
+        Dictionary<string, SortedDictionary<int, string>> texData = new Dictionary<string, SortedDictionary<int, string>>();
+        Dictionary<string, string> animData = new Dictionary<string, string>();
+
+        // Download models, textures, animations
+        public byte[] GetFileData(string url)
+        {
+            // Create an instance of the WebClient class
+            WebClient wc = new WebClient();
+
+            // Download file as byte array
+            return wc.DownloadData(url);
+        }
+
+        // Clean the name from restricted characters
+        public static string CleanName(string name)
+        {
+            if (name.Length <= 0)
+            {
+                throw new Exception("The name must contain at least one character.");
+            }
+
+            name = name.Replace('"', '=');
+            name = name.Replace('*', 'x');
+            name = name.Replace('?', '7');
+            name = name.Replace(":", " -");
+            name = name.Replace('\\', '-');
+            name = name.Replace('/', '-');
+            name = name.Replace('ä', 'a');
+            name = name.Replace('Ä', 'A');
+            name = name.Replace('é', 'e');
+            name = name.Replace('É', 'E');
+            name = name.Replace('Š', 'S');
+            name = name.Replace("|", " -");
+            name = name.Replace('ł', 'l');
+
+            return name;
+        }
+
+        // Download configuration files and return parsed data
+        public dynamic GetConfig(string url, string configType)
+        {
+            // Perform a request to get the file data
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // If the server answered with code 200 - continue.
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                // Define some variables
+                Dictionary<string, dynamic> jsonData;
+                string dataFromStream;
+
+                // Deserialize our data
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    // Read our stream and convert it to UTF-8 string
+                    using (StreamReader reader = new StreamReader(dataStream, Encoding.UTF8))
+                    {
+                        dataFromStream = reader.ReadToEnd();
+                    }
+
+                    // Deserialize
+                    JObject jObject = JObject.Parse(dataFromStream);
+                    jsonData = jObject.ToObject<Dictionary<string, dynamic>>();
+
+                    // Work with the config based on its content
+                    switch (configType)
+                    {
+                        case "model": 
+                            // Set up variables
+                            foreach (KeyValuePair<string, dynamic> kvp in jsonData)
+                            {
+                                switch (kvp.Key)
+                                {
+                                    case "name":
+                                        modelData.Add("modelName", CleanName(kvp.Value.ToString()));
+                                        break;
+
+                                    case "user":
+                                        modelData.Add("userName", kvp.Value.GetValue("username").ToString());
+                                        break;
+
+                                    case "files":
+                                        modelData.Add("osgjsUrl", kvp.Value[0]["osgjsUrl"].ToString());
+                                        break;
+                                }
+                            }
+
+                            // Add additional links
+                            modelData.Add("mfbinUrl", modelData["osgjsUrl"].Replace("file.osgjs.gz", "model_file.bin.gz"));
+                            modelData.Add("mfwbinUrl", modelData["osgjsUrl"].Replace("file.osgjs.gz", "model_file_wireframe.bin.gz"));
+
+                            break;
+
+                        case "textures": 
+                            // Define variables
+                            List<dynamic> texJsonData = new List<dynamic>();
+                            
+                            // Process JSON data
+                            foreach (KeyValuePair<string, dynamic> kvp in jsonData)
+                            {
+                                // Get "results" array and process it
+                                foreach (JObject kvp2 in kvp.Value)
+                                {
+                                    // Add an array with data about each texture to the general array
+                                    texJsonData.Add(kvp2.ToObject<Dictionary<string, dynamic>>());
+                                }
+                            }
+
+                            // Process each texture array
+                            foreach (var texList in texJsonData)
+                            {
+                                // Removing String.Empty gives me error CS0165
+                                string texName = String.Empty;
+
+                                foreach (KeyValuePair<string, dynamic> tex in texList)
+                                {
+                                    if (tex.Key == "name")
+                                    {
+                                        // Convert this value to proper type even if it already stored in this type
+                                        texName = tex.Value.ToString();
+                                    }
+
+                                    else if (tex.Key == "images")
+                                    {
+                                        SortedDictionary<int, string> heightDict = new SortedDictionary<int, string>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+                                        
+                                        if (!texData.ContainsKey(texName))
+                                        {
+                                            texData.Add(texName, heightDict);
+                                        }
+
+                                        foreach (JObject t in tex.Value)
+                                        {
+                                            // Convert these values to proper type even if they are already stored in these types
+                                            int texHeight = Convert.ToInt32(t.GetValue("height"));
+                                            string texUrl = t.GetValue("url").ToString();
+
+                                            // Add the data to the storage
+                                            if (!heightDict.ContainsKey(texHeight))
+                                            {
+                                                heightDict.Add(texHeight, texUrl);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            break;
+
+                        case "anims": 
+                            // Define variables
+                            List<dynamic> animJsonData = new List<dynamic>();
+
+                            // Process JSON data
+                            foreach (KeyValuePair<string, dynamic> kvp in jsonData)
+                            {
+                                // Get "results" array and process it
+                                foreach (JObject kvp2 in kvp.Value)
+                                {
+                                    // Add an array with data about each animation to the general array
+                                    animJsonData.Add(kvp2.ToObject<Dictionary<string, dynamic>>());
+                                }
+                            }
+
+                            // Process each animation array
+                            foreach (var animsList in animJsonData)
+                            {
+                                string animName = String.Empty;
+                                string animUrl = String.Empty;
+
+                                foreach (KeyValuePair<string, dynamic> anim in animsList)
+                                {
+                                    if (anim.Key == "name")
+                                    {
+                                        animName = CleanName(anim.Value.ToString());
+                                    }
+
+                                    if (anim.Key == "url")
+                                    {
+                                        animUrl = anim.Value.ToString();
+                                    }
+                                }
+
+                                if (!animData.ContainsKey(animName))
+                                {
+                                    animData.Add(animName, animUrl);
+                                }
+                            }
+
+                            break;
+                    }
+                }
+
+                // Close the stream to prevent memory leaks.
+                response.Close();
+
+                // Return our data
+                switch (configType)
+                {
+                    case "model":
+                        return modelData;
+                    case "textures":
+                        return texData;
+                    case "anims":
+                        return animData;
+                    default:
+                        return new Dictionary<string, dynamic>();
+                }
+            }
+
+            // Return an error message
+            return "ERROR";
+        }
+    }
+}
---
+++ Source/Program.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+
+namespace SFTool
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Welcome message
+            Console.WriteLine("");
+            Console.WriteLine("   *************************************************   ");
+            Console.WriteLine(" ***************************************************** ");
+            Console.WriteLine(" **                                                 ** ");
+            Console.WriteLine(" **    ╔══════╗╔══════╗╔══════╗             ╔═╗     ** ");
+            Console.WriteLine(" **    ║  ╔═══╝║  ╔═══╝╚═╗  ╔═╝             ║ ║     ** ");
+            Console.WriteLine(" **    ║  ╚═══╗║  ╚═╗    ║  ║ ╔═════╗╔═════╗║ ║     ** ");
+            Console.WriteLine(" **    ╚═══╗  ║║  ╔═╝    ║  ║ ║ ╔═╗ ║║ ╔═╗ ║║ ║     ** ");
+            Console.WriteLine(" **    ╔═══╝  ║║  ║      ║  ║ ║ ╚═╝ ║║ ╚═╝ ║║ ╚╗    ** ");
+            Console.WriteLine(" **    ╚══════╝╚══╝      ╚══╝ ╚═════╝╚═════╝╚══╝    ** ");
+            Console.WriteLine(" **                                                 ** ");
+            Console.WriteLine(" ***************************************************** ");
+            Console.WriteLine("   *************************************************   ");
+            Console.WriteLine("");
+
+            // Print a help text if no arguments were provided
+            if (args.Length < 1)
+            {
+                if (!File.Exists("sftool_links.txt"))
+                {
+                    Console.WriteLine("Usage:");
+                    Console.WriteLine("\t SFTool.exe -h model_hash -o output_path");
+                    Console.WriteLine("\t OR");
+                    Console.WriteLine("\t SFTool.exe -i links_file -o output_path");
+                    Console.WriteLine("");
+                    Console.WriteLine("Arguments:");
+                    Console.WriteLine("\t -h: \t ");
+                    Console.WriteLine("\t     Model hash or whole link.");
+                    Console.WriteLine("\t -i:");
+                    Console.WriteLine("\t     Path to the file with list of links to batch download.");
+                    Console.WriteLine("\t -o: (Optional)");
+                    Console.WriteLine("\t     Path to the output directory. If does not specified, the current directory will used.");
+                    Console.WriteLine("\t     NOTE: -h and -i cannot be used at the same time!");
+                    Console.WriteLine("");
+                    Console.WriteLine("[ERROR] No arguments were provided or file 'sftool_links.txt' was not found.");
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+
+            // ==================================================
+
+            // Start a stopwatch
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // ==================================================
+
+            // Create an instances of required classes
+            ArgumentParser ap = new ArgumentParser();
+
+            // Forming the output path
+            string outputPath = String.Empty;
+
+            if (args.Length < 3)
+            {
+                    outputPath = AppDomain.CurrentDomain.BaseDirectory;
+            }
+            else
+            {
+                if (args[2].StartsWith("-o"))
+                {
+                    outputPath = Path.GetFullPath(args[3]);
+                }
+            }
+
+            // If we have an "-h" argument, just process it as single hash
+            // But if we have an "-l" argument, read a list from specified file and process hashes from it
+            // Or if we have a "sftool_links.txt" file in application directory, both arguments are ignored
+            if (args.Length > 0)
+            {
+                if (args[0].StartsWith("-h"))
+                {
+                    // Get the hash
+                    string hash = ap.Parse(args[1]);
+
+                    // Process the hash
+                    ProcessModel(hash, outputPath);
+                }
+                else if (args[0].StartsWith("-l"))
+                {
+                    // If the file with the list of links exists, then process each line in it
+                    if (File.Exists(args[1]))
+                    {
+                        // Print a message
+                        Console.WriteLine("Processing hashes from file {0}", Path.GetFullPath(args[1]));
+
+                        // Get list of hashes
+                        FileInfo fi = new FileInfo(args[1]);
+                        List<string> links = ap.Parse(fi);
+                        Console.WriteLine("Hashes found: {0}", links.Count);
+
+                        // Process each hash
+                        foreach (string hash in links)
+                        {
+                            ProcessModel(hash, outputPath);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("[ERROR] File '{0}' does not exist.", args[1]);
+                        Console.WriteLine("Press any key to exit.");
+                        Console.ReadLine();
+                    }
+                }
+            }
+            
+            if (File.Exists("sftool_links.txt"))
+            {
+                // Get list of hashes
+                FileInfo fi = new FileInfo("sftool_links.txt");
+                List<string> links = ap.Parse(fi);
+
+                // Process each hash
+                foreach (string hash in links)
+                {
+                    ProcessModel(hash, outputPath);
+                }
+            }
+
+            // ==================================================
+
+            // Stop our stopwatch and print elapsed time
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine("Time taken: {0}", elapsedTime);
+        }
+
+        // Worker
+        public static void ProcessModel(string modelHash, string outputPath)
+        {
+            // Define variables
+            string configBaseUrl = "https://sketchfab.com/i/models/";
+            Dictionary<string, byte[]> files = new Dictionary<string, byte[]>();
+
+            // Create an instances of required classes
+            Downloader dn = new Downloader();
+            GZip gz = new GZip();
+
+            // Print model ID
+            Console.WriteLine("");
+            Console.WriteLine("Got hash: {0} ({1} chars)", modelHash, modelHash.Length);
+
+            // Get filtered config with general model data
+            Dictionary<string, string> modelConfig = dn.GetConfig(configBaseUrl + modelHash, "model");
+            Dictionary<string, SortedDictionary<int, string>> texturesConfig = dn.GetConfig(configBaseUrl + modelHash + "/textures", "textures");
+            Dictionary<string, string> animsConfig = dn.GetConfig(configBaseUrl + modelHash + "/animations", "anims");
+
+            // Check for data availability
+            if (modelConfig.Count > 0)
+            {
+                Console.WriteLine("Got: Model config");
+            }
+
+            if (texturesConfig.Count > 0)
+            {
+                Console.WriteLine("Got: Textures config");
+            }
+
+            if (animsConfig.Count > 0)
+            {
+                Console.WriteLine("Got: Animations config");
+            }
+
+            // Print an short information about requested model
+            Console.WriteLine("Model name: {0} (by {1})", modelConfig["modelName"], modelConfig["userName"]);
+
+            // ==================================================
+
+            // Downloading model
+            byte[] osgjs = dn.GetFileData(modelConfig["osgjsUrl"]);
+            byte[] mfbin = dn.GetFileData(modelConfig["mfbinUrl"]);
+            byte[] mfwbin = dn.GetFileData(modelConfig["mfwbinUrl"]);
+
+            // Decompressing model and storing in resulting array
+            files.Add("file.osgjs", gz.Decompress(osgjs));
+            files.Add("model_file.bin", gz.Decompress(mfbin));
+            files.Add("model_file_wireframe.bin", gz.Decompress(mfwbin));
+
+            // Print a message
+            Console.WriteLine("Model files was successfully downloaded.");
+
+            // ==================================================
+
+            // Processing texture list
+            foreach (KeyValuePair<string, SortedDictionary<int, string>> kvp in texturesConfig)
+            {
+                // Define variables
+                string textureName = kvp.Key;
+                KeyValuePair<int, string> highestTextureData = kvp.Value.First();
+
+                // Print a texture name
+                Console.WriteLine("Got: Texture - {0}", textureName);
+
+                // Get texture file data
+                byte[] texture = dn.GetFileData(highestTextureData.Value);
+
+                // Store our texture in resulting array but first check for duplicate key entry
+                if (!files.ContainsKey(textureName))
+                {
+                    files.Add(textureName, texture);
+                }
+            }
+
+            // ==================================================
+
+            // Processing animations list
+            foreach (KeyValuePair<string, string> kvp in animsConfig)
+            {
+                // Define some variables
+                string animName = kvp.Key;
+
+                // Print an animation name
+                Console.WriteLine("Got: Animation - {0}", animName);
+
+                // Get animation file data
+                byte[] animation = dn.GetFileData(kvp.Value);
+
+                // Store it
+                if (!files.ContainsKey(animName))
+                {
+                    files.Add(animName + ".bin", gz.Decompress(animation));
+                }
+            }
+
+            // ==================================================
+
+            // Preparing output paths
+            string outputDirName = String.Format("{0} (by {1})", modelConfig["modelName"], modelConfig["userName"]);
+            string outputDirPath = Path.Combine(outputPath, outputDirName);
+            string outputModelDir = Path.Combine(outputDirPath, "meshes");
+            string outputTexturesDir = Path.Combine(outputDirPath, "textures");
+            string outputAnimsDir = Path.Combine(outputDirPath, "anims");
+
+            if (!Directory.Exists(outputPath))
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+
+            // Set current path
+            Directory.SetCurrentDirectory(outputPath);
+
+            // Create directories, flush and move the files
+            if (!Directory.Exists(outputDirName))
+            {
+                // Create a main model directory and set it as output
+                Directory.CreateDirectory(outputDirName);
+                Directory.SetCurrentDirectory(outputDirPath);
+
+                // Flush all files
+                foreach (KeyValuePair<string, byte[]> kvp in files)
+                {
+                    File.WriteAllBytes(Path.Combine(outputDirPath, kvp.Key), kvp.Value);
+                }
+
+                // Make separated directories for model, textures and animations and move corresponding files to them
+                Directory.CreateDirectory(outputModelDir);
+
+                // Create a directory only if corresponding config is present
+                if (texturesConfig.Count > 0)
+                {
+                    Directory.CreateDirectory(outputTexturesDir);
+                }
+
+                if (animsConfig.Count > 0)
+                {
+                    Directory.CreateDirectory(outputAnimsDir);
+                }
+
+                // Move model files
+                File.Move("file.osgjs", Path.Combine(outputModelDir, "file.osgjs"));
+                File.Move("model_file.bin", Path.Combine(outputModelDir, "model_file.bin"));
+                File.Move("model_file_wireframe.bin", Path.Combine(outputModelDir, "model_file_wireframe.bin"));
+
+                // Move animations first (because all of them always have the same extension so we can filter them)
+                foreach (FileInfo file in new DirectoryInfo(outputDirPath).GetFiles("*.bin"))
+                {
+                    file.MoveTo($@"{outputAnimsDir}\{file.Name}");
+                }
+
+                // Move all textures
+                foreach (FileInfo file in new DirectoryInfo(outputDirPath).GetFiles("*.*"))
+                {
+                    file.MoveTo($@"{outputTexturesDir}\{file.Name}");
+                }
+
+                // Return to the application directory to prevent recursing
+                Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+                // Print a message
+                Console.WriteLine("Written {0} files.", files.Count);
+            }
+            else
+            {
+                Console.WriteLine("[WARNING] Output directory already exists.");
+            }
+        }
+    }
+
+    public class ArgumentParser
+    {
+        public string Parse(string linkToParse)
+        {
+            return Clean(linkToParse);
+        }
+
+        // I had to add FileInfo here as an argument type, because you cannot use the same type of arguments in method overloads.
+        public List<string> Parse(FileInfo input)
+        {
+            // Define resulting array
+            List<string> result = new List<string>();
+
+            // Perform next operations only if input file is exists.
+            if (input.Exists)
+            {
+                using (StreamReader sr = input.OpenText())
+                {
+                    // Define resulting array
+                    List<string> lines = new List<string>();
+
+                    // Read all lines from our file
+                    string s = String.Empty;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        lines.Add(s);
+                    }
+
+                    // Process each link
+                    foreach (string link in lines)
+                    {
+                        // Clean and check validity of link
+                        string cleanedLink = Clean(link);
+
+                        // Add cleaned hash to the resulting array
+                        result.Add(cleanedLink);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public string Clean(string stringToClean)
+        {
+            if (stringToClean.Contains("https://sketchfab.com"))
+            {
+                // Split the link with "-" as delimiter
+                string[] linkData = stringToClean.Split(Convert.ToChar("-"));
+
+                // Read the last element of the array
+                string modelHash = linkData.Last();
+
+                // Add to the resulting array only if hash is valid
+                if (IsValidHash(modelHash))
+                {
+                    return modelHash;
+                }
+                else
+                {
+                    return "false";
+                }
+            }
+            else
+            {
+                if (IsValidHash(stringToClean))
+                {
+                    return stringToClean;
+                }
+                else
+                {
+                    return "false";
+                }
+            }
+        }
+
+        public bool IsValidHash(string hashToCheck)
+        {
+            if ((hashToCheck.Length < 26) | (hashToCheck.Length > 32))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
